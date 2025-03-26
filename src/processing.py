0775bde0546e70827a0a5a5101ded5316d034c98
Anastasiia Naumova <vsedaleko@gmail.com>
2025-03-26 19:59:09 +0300

    написали функцию filter_by_state для фильтрования операций по статусу

--- src/processing.py
+++ src/processing.py
@@ -12,5 +12,13 @@ print(filter_by_state(([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-0
      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])))
 
 
+def sort_by_date(dictionary_info: List[Dict], ) -> List[Dict]:
+    return sorted(dictionary_info, key=lambda item: item['date'], reverse=True)
+"""определяем функцию для фильтрования операций по статусу"""
 
-
+print(
+    sort_by_date(
+        [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
